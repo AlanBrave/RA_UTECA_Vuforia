83fa9ac128e24928725a0790afb88e8731910ec5
Leonard004 <113481778+Leonard004@users.noreply.github.com>
2023-07-22 23:51:21 -0600

    main - Correcion de algo
    No se porque me marca error pero creo que lo corregí

--- RA-JuegoDeMesa/Assets/Interaction.cs
+++ RA-JuegoDeMesa/Assets/Interaction.cs
@@ -2,59 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum TYPE
+namespace Assets
 {
-    NONE = 0,
-    DEFAULT = 1,
-    PLAYER = 2,
-    ENEMY = 3,
-}
-
-public class Interaction : MonoBehaviour
-{
-    public TYPE type;
-    public GameObject RefObject;
-    public bool IsObjectNear = false;
-    public bool IsInteracting = false;
-    public float DetectionArea = 1.5f;
-    public float InteractionArea = 1.5f;
-    public Vector3 DetectionOffset;
-
-    void FixedUpdate()
+    public enum TYPE
     {
-        IsObjectNearToObject();
-        if (IsObjectInteractingWithObject() && RefObject.GetComponent<Interaction>().type == TYPE.ENEMY)
-        {
-            Debug.Log("Atacking Enemy");
-        }
+        NONE = 0,
+        DEFAULT = 1,
+        PLAYER = 2,
+        ENEMY = 3,
     }
 
-    public bool IsObjectNearToObject()
+    public class Interaction : MonoBehaviour
     {
-        if (Vector3.Distance(RefObject.transform.position, transform.position) <= DetectionArea)
-        {
-            IsInteracting = true;
-            return true;
-        }
-        IsInteracting = false;
-        return false;
-    }
+        public TYPE type;
+        public GameObject RefObject;
+        public bool IsObjectNear = false;
+        public bool IsInteracting = false;
+        public float DetectionArea = 1.5f;
+        public float InteractionArea = 1.5f;
+        public Vector3 DetectionOffset;
 
-    public bool IsObjectInteractingWithObject()
-    {
-        if (Vector3.Distance(RefObject.transform.position, transform.position) <= InteractionArea)
+        void FixedUpdate()
         {
-            IsInteracting = true;
-            return true;
+            IsObjectNearToObject();
+            if (IsObjectInteractingWithObject() && RefObject.GetComponent<Interaction>().type == TYPE.ENEMY)
+            {
+                Debug.Log("Atacking Enemy");
+            }
         }
-        IsInteracting = false;
-        return false;
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(transform.position + DetectionOffset, DetectionArea);
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position + DetectionOffset, InteractionArea);
+
+        public bool IsObjectNearToObject()
+        {
+            if (Vector3.Distance(RefObject.transform.position, transform.position) <= DetectionArea)
+            {
+                IsInteracting = true;
+                return true;
+            }
+            IsInteracting = false;
+            return false;
+        }
+
+        public bool IsObjectInteractingWithObject()
+        {
+            if (Vector3.Distance(RefObject.transform.position, transform.position) <= InteractionArea)
+            {
+                IsInteracting = true;
+                return true;
+            }
+            IsInteracting = false;
+            return false;
+        }
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position + DetectionOffset, DetectionArea);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position + DetectionOffset, InteractionArea);
+        }
     }
 }
